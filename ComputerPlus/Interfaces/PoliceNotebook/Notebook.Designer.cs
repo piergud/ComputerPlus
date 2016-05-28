50ef7963a6c359aa7b31e7b83d05c4093c7beaf3
Fiskey111 <cfiske1@outlook.com>
2016-05-28 01:44:33 -0400

    Add files via upload

---
+++ ComputerPlus/Interfaces/PoliceNotebook/Notebook.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Notsolethalpolicing.MDT
+{
+    partial class NotebookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.InfoLabel1 = new System.Windows.Forms.Label();
+            this.FiskeyLabel = new System.Windows.Forms.Label();
+            this.InfoLabel2 = new System.Windows.Forms.Label();
+            this.MainBox = new System.Windows.Forms.TextBox();
+            this.EraseButton = new System.Windows.Forms.Button();
+            this.YesButton = new System.Windows.Forms.Button();
+            this.NoButton = new System.Windows.Forms.Button();
+            this.Sure = new System.Windows.Forms.Label();
+            this.OpenMDT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.SaveButton.Location = new System.Drawing.Point(269, 507);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(2);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(84, 43);
+            this.SaveButton.TabIndex = 13;
+            this.SaveButton.TabStop = false;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // InfoLabel1
+            // 
+            this.InfoLabel1.AutoSize = true;
+            this.InfoLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.InfoLabel1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.InfoLabel1.Location = new System.Drawing.Point(1, 1);
+            this.InfoLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.InfoLabel1.Name = "InfoLabel1";
+            this.InfoLabel1.Size = new System.Drawing.Size(124, 40);
+            this.InfoLabel1.TabIndex = 17;
+            this.InfoLabel1.Text = "Police Notebook\r\nby Fiskey111";
+            // 
+            // FiskeyLabel
+            // 
+            this.FiskeyLabel.AutoSize = true;
+            this.FiskeyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.FiskeyLabel.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.FiskeyLabel.Location = new System.Drawing.Point(357, 524);
+            this.FiskeyLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.FiskeyLabel.Name = "FiskeyLabel";
+            this.FiskeyLabel.Size = new System.Drawing.Size(198, 24);
+            this.FiskeyLabel.TabIndex = 49;
+            this.FiskeyLabel.Text = "Created by Fiskey111 -- LCPDFR.com\r\nWith input from xAKRONSFINESTx";
+            // 
+            // InfoLabel2
+            // 
+            this.InfoLabel2.AutoSize = true;
+            this.InfoLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.InfoLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.InfoLabel2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.InfoLabel2.Location = new System.Drawing.Point(130, 3);
+            this.InfoLabel2.Name = "InfoLabel2";
+            this.InfoLabel2.Size = new System.Drawing.Size(311, 18);
+            this.InfoLabel2.TabIndex = 120;
+            this.InfoLabel2.Text = "Use this tool to write down anything important.";
+            // 
+            // MainBox
+            // 
+            this.MainBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.MainBox.Location = new System.Drawing.Point(12, 44);
+            this.MainBox.Multiline = true;
+            this.MainBox.Name = "MainBox";
+            this.MainBox.Size = new System.Drawing.Size(543, 458);
+            this.MainBox.TabIndex = 121;
+            // 
+            // EraseButton
+            // 
+            this.EraseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.EraseButton.ForeColor = System.Drawing.Color.Red;
+            this.EraseButton.Location = new System.Drawing.Point(11, 507);
+            this.EraseButton.Margin = new System.Windows.Forms.Padding(2);
+            this.EraseButton.Name = "EraseButton";
+            this.EraseButton.Size = new System.Drawing.Size(145, 43);
+            this.EraseButton.TabIndex = 122;
+            this.EraseButton.TabStop = false;
+            this.EraseButton.Text = "Erase all Notes";
+            this.EraseButton.UseVisualStyleBackColor = true;
+            // 
+            // YesButton
+            // 
+            this.YesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.YesButton.ForeColor = System.Drawing.Color.Red;
+            this.YesButton.Location = new System.Drawing.Point(477, 21);
+            this.YesButton.Margin = new System.Windows.Forms.Padding(2);
+            this.YesButton.Name = "YesButton";
+            this.YesButton.Size = new System.Drawing.Size(78, 23);
+            this.YesButton.TabIndex = 123;
+            this.YesButton.TabStop = false;
+            this.YesButton.Text = "YES";
+            this.YesButton.UseVisualStyleBackColor = true;
+            // 
+            // NoButton
+            // 
+            this.NoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.NoButton.ForeColor = System.Drawing.Color.Black;
+            this.NoButton.Location = new System.Drawing.Point(395, 21);
+            this.NoButton.Margin = new System.Windows.Forms.Padding(2);
+            this.NoButton.Name = "NoButton";
+            this.NoButton.Size = new System.Drawing.Size(78, 23);
+            this.NoButton.TabIndex = 124;
+            this.NoButton.TabStop = false;
+            this.NoButton.Text = "NO";
+            this.NoButton.UseVisualStyleBackColor = true;
+            // 
+            // Sure
+            // 
+            this.Sure.AutoSize = true;
+            this.Sure.BackColor = System.Drawing.Color.Transparent;
+            this.Sure.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.Sure.ForeColor = System.Drawing.Color.Red;
+            this.Sure.Location = new System.Drawing.Point(100, 27);
+            this.Sure.Name = "Sure";
+            this.Sure.Size = new System.Drawing.Size(290, 13);
+            this.Sure.TabIndex = 125;
+            this.Sure.Text = "Has your cursor selected the top leftmost part of the textbox?";
+            // 
+            // OpenMDT
+            // 
+            this.OpenMDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.OpenMDT.Location = new System.Drawing.Point(160, 507);
+            this.OpenMDT.Margin = new System.Windows.Forms.Padding(2);
+            this.OpenMDT.Name = "OpenMDT";
+            this.OpenMDT.Size = new System.Drawing.Size(105, 43);
+            this.OpenMDT.TabIndex = 126;
+            this.OpenMDT.TabStop = false;
+            this.OpenMDT.Text = "Open MDT";
+            this.OpenMDT.UseVisualStyleBackColor = true;
+            // 
+            // NotebookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.OpenMDT);
+            this.Controls.Add(this.Sure);
+            this.Controls.Add(this.NoButton);
+            this.Controls.Add(this.YesButton);
+            this.Controls.Add(this.EraseButton);
+            this.Controls.Add(this.MainBox);
+            this.Controls.Add(this.InfoLabel2);
+            this.Controls.Add(this.FiskeyLabel);
+            this.Controls.Add(this.InfoLabel1);
+            this.Controls.Add(this.SaveButton);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "NotebookForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Police Notebook -- Designed for use with PolicingMDT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Label InfoLabel1;
+        private System.Windows.Forms.Label FiskeyLabel;
+        private System.Windows.Forms.Label InfoLabel2;
+        private System.Windows.Forms.TextBox MainBox;
+        private System.Windows.Forms.Button EraseButton;
+        private System.Windows.Forms.Button YesButton;
+        private System.Windows.Forms.Button NoButton;
+        private System.Windows.Forms.Label Sure;
+        private System.Windows.Forms.Button OpenMDT;
+    }
+}
