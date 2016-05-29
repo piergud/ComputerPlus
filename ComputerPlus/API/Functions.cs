5f3fc8792fe46232a9ae7d4e6f4a9ffe84973a4a
Stealth22PD <stealth22pd@gmail.com>
2016-05-28 19:05:16 -0600

    Added static Functions class to API

---
+++ ComputerPlus/API/Functions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerPlus.API
+{
+    public static class Functions
+    {
+        public static Guid GenerateNewCallID()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
