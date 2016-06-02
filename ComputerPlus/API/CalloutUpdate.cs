6ac901ecf7fb7b5af8611b4c6981f368b6ff143f
Stealth22 <Stealth22PD@users.noreply.github.com>
2016-06-01 20:09:21 -0600

    Adding missing files to project file

---
+++ ComputerPlus/API/CalloutUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerPlus.API
+{
+    public class CalloutUpdate
+    {
+        internal CalloutUpdate(string pText)
+        {
+            TimeAdded = Function.GetMixedDateTime();
+            Text = pText;
+        }
+
+        public DateTime TimeAdded { get; }
+        public string Text { get; }
+    }
+}
