ebf92fdbb6d4c63c93f4dbe95a62ad32baed886b
PieRGud <sumeetjhand@gmail.com>
2016-06-12 04:18:09 -0400

    Marked internal

--- ComputerPlus/UpdateFunction.cs
+++ ComputerPlus/UpdateFunction.cs
@@ -4,10 +4,10 @@ using Rage;
 
 namespace ComputerPlus
 {
-    public class UpdateFunction
+    internal static class UpdateFunction
     {
         protected const string UpdateEndpoint = "http://www.lcpdfr.com/applications/downloadsng/interface/api.php?do=checkForUpdates&fileId=11453&beta=false&textOnly=true";
-        public static string GetLatestVersion(int fileId, bool betaBranch = false)
+        internal static string GetLatestVersion(int fileId, bool betaBranch = false)
         {
             string latestVersion = null;
             try
