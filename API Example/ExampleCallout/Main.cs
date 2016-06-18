60206d1e5fbc05322b7ae3cb908cfc82330a346b
PieRGud <sumeetjhand@gmail.com>
2016-06-18 18:37:19 -0400

    Added API example

---
+++ API Example/ExampleCallout/Main.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using LSPD_First_Response.Mod.API;
+using Rage;
+
+namespace Example
+{
+    public class Main : Plugin
+    {
+        public override void Initialize()
+        {
+            Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(LSPDFRResolveEventHandler);
+            Game.LogTrivial("Example loaded.");
+            Game.FrameRender += Process;
+        }
+
+        public override void Finally()
+        {
+        }
+
+        public static Assembly LSPDFRResolveEventHandler(object sender, ResolveEventArgs args)
+        {
+            foreach (Assembly assembly in Functions.GetAllUserPlugins())
+            {
+                if (args.Name.ToLower().Contains(assembly.GetName().Name.ToLower()))
+                {
+                    return assembly;
+                }
+            }
+            return null;
+        }
+
+        private static void OnOnDutyStateChangedHandler(bool OnDuty)
+        {
+            if (OnDuty)
+            {
+                RegisterCallouts();
+            }
+        }
+
+        private static void RegisterCallouts()
+        {
+            Functions.RegisterCallout(typeof(ExampleCallout));
+        }
+
+        private static void Process(object sender, GraphicsEventArgs e)
+        {
+            if (Game.IsKeyDown(Keys.G))
+            {
+                Functions.StartCallout("Example Callout");
+            }
+        }
+    }
+}
