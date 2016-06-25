f8194b897dbed30c4246bcee969b298d1bbbcaca
ainesophaur <tsheldon88@outlook.com>
2016-06-25 03:29:48 -0400

    Includes integrators to provide forms through api

---
+++ ComputerPlus/api/ExternalUI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rage.Forms;
+
+namespace ComputerPlus.API
+{
+    public sealed class ExternalUI
+    {
+        public Guid Identifier {
+            get;
+            internal set;
+        }
+
+        public String DisplayName
+        {
+            get;
+            internal set;
+        }
+
+        public String Author
+        {
+            get;
+            internal set;
+        }
+
+        internal Func<GwenForm> Creator
+        {
+            get;
+            private set;
+        }   
+        
+        internal Action OnOpen
+        {
+            get;
+            private set;
+        }         
+        
+        internal ExternalUI(Guid identifier, String displayName, String author, Func<GwenForm> creator, Action onOpen = null)
+        {
+            this.Identifier = identifier;
+            this.DisplayName = displayName;
+            this.Author = author;
+            this.Creator = creator;
+            this.OnOpen = onOpen;
+        }
+    }
+}
---
+++ ExampleExternalUI/ExampleForm.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Drawing;
+using System.Linq;
+using Rage;
+using Rage.Forms;
+using Gwen.Control;
+
+
+namespace ExampleExternalUI
+{
+    class ExampleForm : GwenForm
+    {
+        private Button button1;
+
+        public ExampleForm() : base(typeof(ExampleFormTemplate))
+        {
+
+        }
+
+        public override void InitializeLayout()
+        {
+            button1.Clicked += Button1_Clicked;
+        }
+
+        private void Button1_Clicked(Base sender, ClickedEventArgs arguments)
+        {
+            Game.DisplayHelp("Hey you clicked me");
+        }
+    }
+}
