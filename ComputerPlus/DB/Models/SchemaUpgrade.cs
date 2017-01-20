152094492b48552ec275450bdbadcc0ce8996a7e
ainesophaur <tsheldon88@outlook.com>
2017-01-19 19:38:15 -0500

    SQLite addition

---
+++ ComputerPlus/DB/Models/SchemaUpgrade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Data.SQLite;
+
+namespace ComputerPlus.DB.Models
+{
+    [Serializable()]
+    [XmlRoot("SchemaUpgrades")]
+    public class SchemaUpgrade
+    {
+        [XmlElement]
+        public List<SchemaVersion> Versions;
+    }
+
+    public class SchemaVersion : BaseModel
+    {
+        [XmlAttribute(AttributeName = "plans")]
+        public List<String> Plans;
+
+        public static SchemaVersion Create(String id)
+        {
+            return new SchemaVersion() { id = id };
+        }
+    }
+
+}
---
+++ ComputerPlus/DB/Storage.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+using ComputerPlus.DB.Models;
+
+namespace ComputerPlus.DB
+{
+    public enum UpgradeStatus { MISSING_SCHEMAS, COMPLETED, BAD_CONNECTION, FAILED };
+    class Storage
+    {
+        private static readonly String DB_FILE_NAME = Function.GetAssetPath("reports.sqlite");
+        private static readonly String CONNECTION_STRING = String.Format("Data Source={0};Version=3;FKSupport=True;", DB_FILE_NAME);
+
+        Storage()
+        {
+        }
+
+        private void SqlConnectionDisposed(object sender, EventArgs e)
+        {
+        }
+
+        ~Storage()
+        {
+        }
+
+        public SQLiteConnection Connection()
+        {
+            return new SQLiteConnection(CONNECTION_STRING);
+        }
+
+        public void Close()
+        {
+            //if (m_dbOpened)
+            //{
+            //    dbConnection.Close();
+            //}
+        }
+
+        internal UpgradeStatus Upgrade(SchemaVersion toVersion)
+        {
+            using (var db = this.Connection().OpenAndReturn())
+            {                
+                if (!toVersion.Plans.All(x => File.Exists(Function.GetAssetPath(String.Format(@"Schemas\{0}.sql", x))))) return UpgradeStatus.MISSING_SCHEMAS;
+                using (SQLiteCommand cmd = new SQLiteCommand(db))
+                using (SQLiteTransaction transaction = db.BeginTransaction())
+                {
+                    try
+                    {
+                        foreach (var exec in toVersion.Plans.Select(x => File.ReadAllText(Function.GetAssetPath(String.Format(@"Schemas\{0}.sql", x)))))
+                        {
+                            cmd.CommandText = exec;
+                            cmd.ExecuteNonQuery();
+                        }
+                        transaction.Commit();
+                        return UpgradeStatus.COMPLETED;
+                    }
+                    catch (SQLiteException e)
+                    {
+                        Function.Log(String.Format("Failed upgrade schema:{0}{1}", Environment.NewLine, e.Message));
+                        return UpgradeStatus.FAILED;
+                    }
+                }
+            }
+            
+        }
+
+        public static Storage ReadOrInit()
+        {
+            return File.Exists(DB_FILE_NAME) ? new Storage() : Storage.InitNew();
+        }
+
+        public static Storage InitNew()
+        {
+            SQLiteConnection.CreateFile(DB_FILE_NAME);
+            return new Storage();
+        }
+    }
+}
